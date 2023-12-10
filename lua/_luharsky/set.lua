7fc657c2c96f7e5536eb50a35f4d66be2bcfda2a
ANDRII POPENKO <andriipopenko@ANDRIIs-MacBook-Pro.local>
2023-12-10 17:36:12 +0100

    global options has been improved

--- lua/_luharsky/set.lua
+++ lua/_luharsky/set.lua
@@ -1,21 +1,45 @@
+-- cursor
 vim.opt.guicursor = "i-ci:ver30-iCursor-blinkwait300-blinkon200-blinkoff150";
+vim.opt.cursorline = true
+
+-- line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = false
+
+-- tabs & indention
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = false
 vim.opt.smartindent = true
+
+-- search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+
+-- appearance
+vim.opt.signcolumn = "yes"
+vim.opt.termguicolors = true
+
+-- line wrapping
 vim.opt.wrap = false
+
+
+--  backspace
+vim.opt.backspace = "indent,eol,start"
+
+-- clipboard
+vim.opt.clipboard:append("unnamedplus")
+
+-- undodir & backup
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
-vim.opt.incsearch = true
-vim.opt.hlsearch = true
-vim.opt.termguicolors = false
+
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "0"
